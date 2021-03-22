4f6177bb9953f71304dc86f50fdd0bb47d6891f1
Igor Matias <igorhmatias@gmail.com>
2021-03-22 19:22:36 -0300

    feat: criando component header

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
+import React from "react";
+import { Header } from "./components/Header";
+import { GlobalStyle } from "./styles/global";
 
 export function App() {
   return (
-    <div className="App">
-      <h1>Hello Ignite</h1>
-    </div>
+    <>
+      <GlobalStyle />
+      <Header />
+   
+    </>
   );
 }
 
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -3,7 +3,7 @@ import {createGlobalStyle} from 'styled-components'
 export const GlobalStyle = createGlobalStyle`
 :root {
     --background: #f0f2f5;
-    --red #E52E4D;
+    --red: #E52E4D;
     --blue: #5429CC;
 
     --blue-light: #6933FF;
