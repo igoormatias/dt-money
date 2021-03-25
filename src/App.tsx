a48337326fab80468857dbe08303c0ae6e4f3268
Igor Matias <igorhmatias@gmail.com>
2021-03-25 14:40:09 -0300

    feat: importando dashboard component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
 
@@ -7,6 +8,7 @@ export function App() {
     <>
       <GlobalStyle />
       <Header />
+      <Dashboard />
    
     </>
   );
