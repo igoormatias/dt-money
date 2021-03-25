90e068b5e77955744457b7be5befe7b12edd1fc2
Igor Matias <igorhmatias@gmail.com>
2021-03-25 18:00:43 -0300

    feat: configurando miragejs

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createServer } from 'miragejs';
 import { App } from './App';
 
+
+
+createServer({
+  routes(){
+    this.namespace = 'api';
+
+    this.get('/transactions', ()=> {
+      return [
+        {
+          id:1,
+          title: 'Transactions 1',
+          amount: 400,
+          type: 'deposit',
+          category: 'Food',
+          createdAt: Date()
+
+        }
+      ]
+    })
+  }
+})
 
 
 
