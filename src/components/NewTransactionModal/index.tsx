4efdefccd6371a6291feff314d90109aecfdf0ed
Igor Matias <igorhmatias@gmail.com>
2021-05-01 12:08:45 -0300

    feat: estilizando botões

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,8 @@
 import Modal from 'react-modal';
 import closeImg from '../../assets/close.svg';
-import { Container } from './styles'
+import incomeImg from '../../assets/income.svg';
+import outcomeImg from '../../assets/outcome.svg';
+import { Container,TransactionTypeContainer } from './styles'
 
 interface NewTransactionModalProps {
     isOpen: boolean;
@@ -32,6 +34,17 @@ export function NewTransactionModal({isOpen, onRequestClose} :NewTransactionModa
             type="number"
              placeholder="Valor"
             />
+
+            <TransactionTypeContainer>
+                <button type="button">
+                    <img src={incomeImg} alt="Entrada"/>
+                    <span>Entrada</span>
+                </button>
+                <button type="button">
+                    <img src={outcomeImg} alt="Saída"/>
+                    <span>Saída</span>
+                </button>
+            </TransactionTypeContainer>
             <input
             placeholder="Categoria"
             />
