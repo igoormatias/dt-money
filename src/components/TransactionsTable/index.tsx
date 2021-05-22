0f93deaba119fbd3b56fcd5651ecdbb1c05504da
Igor Matias <igorhmatias@gmail.com>
2021-05-22 11:03:39 -0300

    fix: ajustando dados da tabela com campos corretos

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -23,11 +23,11 @@ export function TransactionsTable() {
                 { transactions.map(transaction => (
                     <tr key={transaction.id}>
                     <td>{transaction.title}</td>
-                    <td className={transaction.type}>{transaction.type}</td>
-                    <td> {new Intl.NumberFormat('pt-BR', {
+                    <td className={transaction.type}>{new Intl.NumberFormat('pt-BR', {
                         style: 'currency',
                         currency: 'BRL'
                     }).format(transaction.amount)}</td>
+                    
                    <td>{transaction.category}</td>
                     <td>{ new Intl.DateTimeFormat('pt-BR').format(
                         new Date(transaction.createdAt)
