1c77a0734897cfe8176cbca36a30cbc04be61066
Igor Matias <igorhmatias@gmail.com>
2021-05-22 11:23:39 -0300

    feat: calculando totais no summary

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -7,33 +7,66 @@ import { TransactionsContext } from '../../TransactionsContext';
 import { Container } from "./styles";
 
 export function Summary() {
+    const { transactions } = useContext(TransactionsContext);
 
-    const {transactions} = useContext(TransactionsContext);
+    const summary = transactions.reduce((acc, transaction) => {
+        if (transaction.type === 'deposit') {
+            acc.deposits += transaction.amount;
+            acc.total += transaction.amount
+        } else {
+            acc.withdraw += transaction.amount;
+            acc.total += transaction.amount
+        }
 
-    console.log(transactions);
+        return acc;
+    }, {
+        deposits: 0,
+        withdraw: 0,
+        total: 0
+    });
+
+
+
+
     return (
-       <Container>
-           <div>
-               <header>
-                   <p>Entradas</p>
-                   <img src={incomeImg} alt="Entradas"/>
-               </header>
-               <strong>R$1000,00</strong>
-           </div>
-           <div>
-               <header>
-                   <p>Saidas</p>
-                   <img src={outcomeImg} alt="Saidas"/>
-               </header>
-               <strong>R$1000,00</strong>
-           </div>
-           <div className="highlight-background">
-               <header>
-                   <p>Total</p>
-                   <img src={totalImg} alt="Total"/>
-               </header>
-               <strong>-R$10,00</strong>
-           </div>
-         </Container>
+        <Container>
+            <div>
+                <header>
+                    <p>Entradas</p>
+                    <img src={incomeImg} alt="Entradas" />
+                </header>
+                <strong>
+                    {new Intl.NumberFormat('pt-BR', {
+                        style: 'currency',
+                        currency: 'BRL'
+                    }).format(summary.deposits)}
+                </strong>
+            </div>
+            <div>
+                <header>
+                    <p>Saidas</p>
+                    <img src={outcomeImg} alt="Saidas" />
+                </header>
+                <strong>
+                    -
+                   {new Intl.NumberFormat('pt-BR', {
+                    style: 'currency',
+                    currency: 'BRL'
+                }).format(summary.withdraw)}
+                </strong>
+            </div>
+            <div className="highlight-background">
+                <header>
+                    <p>Total</p>
+                    <img src={totalImg} alt="Total" />
+                </header>
+                <strong>
+                    {new Intl.NumberFormat('pt-BR', {
+                        style: 'currency',
+                        currency: 'BRL'
+                    }).format(summary.total)}
+                </strong>
+            </div>
+        </Container>
     )
 }
