c568ee6c7a56d97aa9af97a3252d8e31c8814ec6
Igor Matias <igorhmatias@gmail.com>
2021-05-22 11:36:56 -0300

    feat: trocando useContext por useTransactions

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,6 @@
-import { useState, FormEvent, useContext } from 'react';
+import { useState, FormEvent } from 'react';
 import Modal from 'react-modal';
-import { TransactionsContext } from '../../TransactionsContext';
+import { useTransactions } from '../../hooks/useTransactions'
 
 import closeImg from '../../assets/close.svg';
 import incomeImg from '../../assets/income.svg';
@@ -13,7 +13,7 @@ interface NewTransactionModalProps {
     onRequestClose: () => void;
 }
 export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionModalProps) {
-    const { createTransaction } = useContext(TransactionsContext)
+    const { createTransaction } = useTransactions()
 
     const [type, setType] = useState('deposit');
     const [amount, setAmount] = useState(0);
